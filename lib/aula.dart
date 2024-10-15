89f11160fe5ffcd59ce3716e856840bc71b63e42
Gustavo dos Santos Capeletti <gustavocapeletti007@gmail.com>
2024-10-14 21:45:49 -0300

    Tela de exercicios e aulas

--- lib/aula.dart
+++ lib/aula.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/cadastro.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AulaStl extends StatelessWidget {
   const AulaStl({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -15,152 +15,172 @@ class AulaStl extends StatelessWidget {
 
 class AulaStf extends StatefulWidget {
   const AulaStf({super.key});
+
   @override
   State<AulaStf> createState() => _AulaStfState();
 }
 
 class _AulaStfState extends State<AulaStf> {
   final TextEditingController _nomeController = TextEditingController();
-  final aulaStream = Supabase.instance.client.from('aulas').stream(primaryKey: ['id']);
+  List<dynamic> aulas = []; // Lista para armazenar as aulas
 
-  Future<void> singOut() async {
-    await Supabase.instance.client.auth.signOut();
-    if (!mounted) return;
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => const CadastroStl()),
-    );
+  @override
+  void initState() {
+    super.initState();
+    _loadAulas(); // Carrega as aulas ao iniciar
   }
 
   Future<void> novaAula() async {
     final nome = _nomeController.text.trim();
+    
     if (nome.isNotEmpty) {
-      await Supabase.instance.client.from('aulas').insert({'nome': nome});
-      _nomeController.clear();
+      final response = await Supabase.instance.client.from('aulas').insert({
+        'nome': nome,
+      }).execute();
+
+      if (response.status == 200) {
+        _loadAulas(); // Atualiza a lista de aulas
+        _nomeController.clear();
+        _loadAulas();
+      } else {
+        print('Erro ao inserir aula: ');
+        _loadAulas();
+      }
     }
   }
 
-  Future<void> atualizarAula(aulaid, novoTexto) async{
-    await Supabase.instance.client.from('aulas').update({'nome' : novoTexto}).eq('id', aulaid);
+  Future<void> _loadAulas() async {
+    final response = await Supabase.instance.client.from('aulas').select().execute();
+    if (response.status == 200) {
+      setState(() {
+        aulas = response.data; // Armazena as aulas na lista
+      });
+    } else {
+      print('Erro ao carregar aulas:');
+    }
   }
 
-  Future<void> deletarAula(String aulaid) async {
-    await Supabase.instance.client.from('aulas').delete().eq('id', aulaid);
+  Future<void> atualizarAula(String aulaId, String novoNome) async {
+    await Supabase.instance.client.from('aulas').update({'nome': novoNome}).eq('id', aulaId).execute();
+    _loadAulas(); // Atualiza a lista após a edição
+  }
+
+  Future<void> deletarAula(String aulaId) async {
+    await Supabase.instance.client.from('aulas').delete().eq('id', aulaId).execute();
+    _loadAulas(); // Atualiza a lista após a exclusão
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(
-          child: Text(
-            'MELP',
-            style: TextStyle(color: Colors.white),
-          ),
-        ), 
-        backgroundColor: Colors.black,
+        title: const Text('Adicionar Aula'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            Row(mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-              SizedBox(
-                width: 500,
-                child: TextField(
-                  controller: _nomeController,
-                  cursorColor: Colors.black,
-                  decoration: const InputDecoration(
-                    hintText: 'Insira o nome da aula',
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.menu_book_rounded),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(6)),
-                      borderSide: BorderSide(
-                        color: Color.fromARGB(179, 143, 137, 137),
+            TextField(
+              controller: _nomeController,
+              decoration: const InputDecoration(
+                hintText: 'Nome da Aula',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: novaAula,
+              child: const Text('Adicionar Aula'),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: aulas.length,
+                itemBuilder: (context, index) {
+                  final aula = aulas[index];
+                  final aulaId = aula['id'].toString();
+
+                  return Card(
+                    margin: const EdgeInsets.symmetric(vertical: 8),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            aula['nome'],
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          Row(
+                            children: [
+                              IconButton(
+                                icon: const Icon(Icons.edit),
+                                onPressed: () {
+                                  // Mostrar diálogo para editar a aula
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      final nomeController = TextEditingController(text: aula['nome']);
+                                      return AlertDialog(
+                                        title: const Text('Editar Aula'),
+                                        content: TextField(
+                                          controller: nomeController,
+                                          decoration: const InputDecoration(hintText: 'Nome da Aula'),
+                                        ),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              // Atualizar a aula
+                                              atualizarAula(aulaId, nomeController.text);
+                                              Navigator.pop(context);
+                                            },
+                                            child: const Text('Salvar'),
+                                          ),
+                                          TextButton(
+                                            onPressed: () => Navigator.pop(context),
+                                            child: const Text('Cancelar'),
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                },
+                              ),
+                              IconButton(
+                                icon: const Icon(Icons.delete),
+                                onPressed: () async {
+                                  bool deletedConfirmed = await showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        title: const Text('Deletar Aula'),
+                                        content: const Text('Você tem certeza?'),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.pop(context, false);
+                                            },
+                                            child: const Text('Cancelar'),
+                                          ),
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.pop(context, true);
+                                            },
+                                            child: const Text('Deletar'),
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                  if (deletedConfirmed) {
+                                    await deletarAula(aulaId);
+                                  }
+                                },
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
                     ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(6)),
-                      borderSide: BorderSide(
-                        color: Color.fromARGB(179, 143, 137, 137),
-                      ),
-                    )
-                  ),
-                ),
-              ),
-
-              ElevatedButton(
-                onPressed: novaAula,
-                child: const Text('Criar'),
-              ),
-
-              ElevatedButton(
-                onPressed: singOut,
-                child: const Text('Sair'),
-              ),
-            ],),
-            
-            const SizedBox(height: 32),
-            Expanded(
-              child: StreamBuilder<List<Map<String, dynamic>>>(
-                stream: aulaStream,
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else {
-                    final aulas = snapshot.data!;
-                    return ListView.builder(
-                      itemCount: aulas.length,
-                      itemBuilder: (context, index) {
-                        final aula = aulas[index];
-                        final aulaId = aula['id'].toString();
-                        return ListTile(
-                          title: Text(aula['nome'] ?? 'Nome não encontrado'),
-                          trailing: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              IconButton(onPressed: (){
-                                showDialog(context: context, builder: (context){
-                                  return SimpleDialog(
-                                    title: const Text("Editar nota"),
-                                    children: [
-                                      TextFormField(
-                                        initialValue: aula['nome'],
-                                        onFieldSubmitted: (value) async{
-                                          await atualizarAula(aulaId, value);
-                                        }),
-                                    ],
-                                  );
-                                });
-                              }, icon: const Icon(Icons.edit)),
-                              IconButton(onPressed: () async{
-                                bool deletedConfirmed = await showDialog(context: context, builder: (context){
-                                  return AlertDialog(
-                                    title: const Text('Deletar aula'),
-                                    content: const Text('Você tem certeza?'),
-                                    actions: [
-                                      TextButton(onPressed: (){
-                                        Navigator.pop(context, false);
-                                      }, child: const Text('Cancelar')),
-                                      TextButton(onPressed: (){
-                                        Navigator.pop(context, true);
-                                      }, child: const Text('Deletar'))
-                                    ],
-                                    
-                                  );
-                                });
-                                if(deletedConfirmed){
-                                  await deletarAula(aulaId);
-                                }
-                              }, icon: const Icon(Icons.delete))
-                            ],
-                            ),
-                        );
-                      },
-                    );
-                  }
+                  );
                 },
               ),
             ),
--- lib/exercicio.dart
+++ lib/exercicio.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ExercicioStl extends StatelessWidget {
   const ExercicioStl({super.key});
+  
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -13,15 +15,252 @@ class ExercicioStl extends StatelessWidget {
 
 class ExercicioStf extends StatefulWidget {
   const ExercicioStf({super.key});
+  
   @override
   State<ExercicioStf> createState() => _ExercicioStfState();
 }
 
 class _ExercicioStfState extends State<ExercicioStf> {
+  final TextEditingController _enunciadoController = TextEditingController();
+  final TextEditingController _opcaoAController = TextEditingController();
+  final TextEditingController _opcaoBController = TextEditingController();
+  final TextEditingController _opcaoCController = TextEditingController();
+  final TextEditingController _opcaoDController = TextEditingController();
+  List<dynamic> exercicios = []; // Lista para armazenar os exercícios
+
+  @override
+  void initState() {
+    super.initState();
+    _loadExercicios(); // Carrega os exercícios ao iniciar
+  }
+
+  Future<void> novoExercicio() async {
+    final enunciado = _enunciadoController.text.trim();
+    final opcaoA = _opcaoAController.text.trim();
+    final opcaoB = _opcaoBController.text.trim();
+    final opcaoC = _opcaoCController.text.trim();
+    final opcaoD = _opcaoDController.text.trim();
+    
+    final response = await Supabase.instance.client.from('exercicio').insert({
+      'enunciado': enunciado, 
+      'opcaoA': opcaoA, 
+      'opcaoB': opcaoB, 
+      'opcaoC': opcaoC, 
+      'opcaoD': opcaoD
+    }).execute();
+
+    if (response.status == 200) {
+      _loadExercicios(); // Atualiza a lista de exercícios
+      _enunciadoController.clear();
+      _opcaoAController.clear();
+      _opcaoBController.clear();
+      _opcaoCController.clear();
+      _opcaoDController.clear();
+      _loadExercicios(); 
+    } else {
+      print('Erro ao inserir exercício:');
+      _loadExercicios(); 
+    }
+  }
+
+  Future<void> _loadExercicios() async {
+    final response = await Supabase.instance.client.from('exercicio').select().execute();
+    if (response.status == 200) {
+      setState(() {
+        exercicios = response.data; // Armazena os exercícios na lista
+      });
+    } else {
+      print('Erro ao carregar exercícios: ');
+    }
+  }
+
+  Future<void> atualizarExercicio(String exercicioId, Map<String, dynamic> dadosAtualizados) async {
+    await Supabase.instance.client.from('exercicio').update(dadosAtualizados).eq('id', exercicioId).execute();
+    _loadExercicios(); // Atualiza a lista após a edição
+  }
+
+  Future<void> deletarExercicio(String exercicioId) async {
+    await Supabase.instance.client.from('exercicio').delete().eq('id', exercicioId).execute();
+    _loadExercicios(); // Atualiza a lista após a exclusão
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Execericio'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Adicionar Exercício'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _enunciadoController,
+              decoration: const InputDecoration(
+                hintText: 'Enunciado',
+              ),
+            ),
+            TextField(
+              controller: _opcaoAController,
+              decoration: const InputDecoration(
+                hintText: 'Opcao A',
+              ),
+            ),
+            TextField(
+              controller: _opcaoBController,
+              decoration: const InputDecoration(
+                hintText: 'Opcao B',
+              ),
+            ),
+            TextField(
+              controller: _opcaoCController,
+              decoration: const InputDecoration(
+                hintText: 'Opcao C',
+              ),
+            ),
+            TextField(
+              controller: _opcaoDController,
+              decoration: const InputDecoration(
+                hintText: 'Opcao D',
+              ),
+            ),
+            ElevatedButton(onPressed: novoExercicio, child: const Text('Enviar')),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: exercicios.length,
+                itemBuilder: (context, index) {
+                  final exercicio = exercicios[index];
+                  final exercicioId = exercicio['id'].toString();
+
+                  return Card(
+                    margin: const EdgeInsets.symmetric(vertical: 8),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            exercicio['enunciado'],
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 8),
+                          Text('A: ${exercicio['opcaoA']}'),
+                          Text('B: ${exercicio['opcaoB']}'),
+                          Text('C: ${exercicio['opcaoC']}'),
+                          Text('D: ${exercicio['opcaoD']}'),
+                          const SizedBox(height: 8),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              IconButton(
+                                icon: const Icon(Icons.edit),
+                                onPressed: () {
+                                  // Mostrar dialogo para editar o exercício
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      final enunciadoController = TextEditingController(text: exercicio['enunciado']);
+                                      final opcaoAController = TextEditingController(text: exercicio['opcaoA']);
+                                      final opcaoBController = TextEditingController(text: exercicio['opcaoB']);
+                                      final opcaoCController = TextEditingController(text: exercicio['opcaoC']);
+                                      final opcaoDController = TextEditingController(text: exercicio['opcaoD']);
+                                      
+                                      return AlertDialog(
+                                        title: const Text('Editar Exercício'),
+                                        content: Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            TextField(
+                                              controller: enunciadoController,
+                                              decoration: const InputDecoration(hintText: 'Enunciado'),
+                                            ),
+                                            TextField(
+                                              controller: opcaoAController,
+                                              decoration: const InputDecoration(hintText: 'Opcao A'),
+                                            ),
+                                            TextField(
+                                              controller: opcaoBController,
+                                              decoration: const InputDecoration(hintText: 'Opcao B'),
+                                            ),
+                                            TextField(
+                                              controller: opcaoCController,
+                                              decoration: const InputDecoration(hintText: 'Opcao C'),
+                                            ),
+                                            TextField(
+                                              controller: opcaoDController,
+                                              decoration: const InputDecoration(hintText: 'Opcao D'),
+                                            ),
+                                          ],
+                                        ),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              // Atualizar o exercício
+                                              atualizarExercicio(exercicioId, {
+                                                'enunciado': enunciadoController.text,
+                                                'opcaoA': opcaoAController.text,
+                                                'opcaoB': opcaoBController.text,
+                                                'opcaoC': opcaoCController.text,
+                                                'opcaoD': opcaoDController.text,
+                                              });
+                                              Navigator.pop(context);
+                                            },
+                                            child: const Text('Salvar'),
+                                          ),
+                                          TextButton(
+                                            onPressed: () => Navigator.pop(context),
+                                            child: const Text('Cancelar'),
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                },
+                              ),
+                              IconButton(
+                                icon: const Icon(Icons.delete),
+                                onPressed: () async {
+                                  bool deletedConfirmed = await showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        title: const Text('Deletar Exercício'),
+                                        content: const Text('Você tem certeza?'),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.pop(context, false);
+                                            },
+                                            child: const Text('Cancelar'),
+                                          ),
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.pop(context, true);
+                                            },
+                                            child: const Text('Deletar'),
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                  if (deletedConfirmed) {
+                                    await deletarExercicio(exercicioId);
+                                  }
+                                },
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
